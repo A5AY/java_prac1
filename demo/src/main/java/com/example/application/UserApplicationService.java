1ba339b5ba6d4aa8e8a488a9fceb9d7c42fab25b
eseo_kun <eseo_kun@example.com>
2025-06-05 23:34:20 +0900

    p62まで

---
+++ demo/src/main/java/com/example/application/UserApplicationService.java
@@ -0,0 +1,17 @@
+package com.example.application;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserApplicationService {
+    
+    public Map<String,Integer> getGenderMap() {
+        Map<String,Integer> genderMap = new LinkedHashMap<>();
+        genderMap.put("Male", 1);
+        genderMap.put("Female", 2);
+        return genderMap;
+    }
+}
