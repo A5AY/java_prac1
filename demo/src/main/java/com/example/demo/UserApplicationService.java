43c925af8c2d25d345247fc6629571baf6a66761
eseo_kun <eseo_kun@example.com>
2025-06-07 12:53:31 +0900

    messages.propertiesを追加

--- demo/src/main/java/com/example/demo/UserApplicationService.java
+++ demo/src/main/java/com/example/demo/UserApplicationService.java
@@ -1,17 +1,25 @@
 package com.example.demo;
 
 import java.util.LinkedHashMap;
+import java.util.Locale;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserApplicationService {
+    @Autowired
+    private MessageSource messageSource;
     
     public Map<String,Integer> getGenderMap() {
         Map<String,Integer> genderMap = new LinkedHashMap<>();
-        genderMap.put("Male", 1);
-        genderMap.put("Female", 2);
+        String male = messageSource.getMessage("male", null, Locale.JAPAN);
+        String female = messageSource.getMessage("female", null, Locale.JAPAN);
+        
+        genderMap.put(male, 1);
+        genderMap.put(female,2);
         return genderMap;
     }
 }
